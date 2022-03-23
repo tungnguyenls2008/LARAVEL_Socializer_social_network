620c1bf733a1e190e1e88c10bb88dfda0661dd24
tungnd5 <tungnd5@revex.vn>
2022-03-23 16:49:07 +0700

    fix input model

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Input;
 use Response;
 use App\User;
 Use Auth;
